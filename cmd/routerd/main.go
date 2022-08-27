0412b8bb19cc190bd296be0dbf1e2eb59fe87b26
Nate Maninger <me@n8m.us>
2022-08-27 16:30:47 -0600

    unnecessary channel

--- cmd/routerd/main.go
+++ cmd/routerd/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"path/filepath"
 	"strings"
 	"time"
@@ -19,39 +18,25 @@ func main() {
 	}
 	defer indexPage.Close()
 
-	log.Println("server started")
+	err = http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasPrefix(r.URL.Path, "/") {
+			r.URL.Path = "/" + r.URL.Path
+		}
+		if r.URL.Path == "/" {
+			http.ServeContent(w, r, "index.html", time.Time{}, indexPage)
+			return
+		}
 
-	errChan := make(chan error, 1)
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		<-c
-		errChan <- nil
-	}()
-
-	go func() {
-		errChan <- http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !strings.HasPrefix(r.URL.Path, "/") {
-				r.URL.Path = "/" + r.URL.Path
-			}
-			if r.URL.Path == "/" {
-				http.ServeContent(w, r, "index.html", time.Time{}, indexPage)
-				return
-			}
-
-			path := filepath.Clean(filepath.Join(base, r.URL.Path))
-			f, err := os.Open(path)
-			if err != nil {
-				http.ServeContent(w, r, "index.html", time.Time{}, indexPage)
-				return
-			}
-			defer f.Close()
-			http.ServeContent(w, r, r.URL.Path, time.Time{}, f)
-		}))
-	}()
-
-	if err := <-errChan; err != nil && !errors.Is(err, http.ErrServerClosed) {
+		path := filepath.Clean(filepath.Join(base, r.URL.Path))
+		f, err := os.Open(path)
+		if err != nil {
+			http.ServeContent(w, r, "index.html", time.Time{}, indexPage)
+			return
+		}
+		defer f.Close()
+		http.ServeContent(w, r, r.URL.Path, time.Time{}, f)
+	}))
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("server error: %s", err)
 	}
 }
